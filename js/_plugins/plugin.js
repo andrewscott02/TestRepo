934174ba1ea3d12ade018e5c1af087de436b321d
Andrew Scott <andrewscott02@icloud.com>
2024-03-26 09:04:50 +0000

    Sticky navbar and carousel plugins

---
+++ js/_plugins/plugin.js
@@ -0,0 +1,25 @@
+// $(".animsition").animsition({
+//     inClass: "fade-in-right-lg",
+//     outClass: "fade-out-right-lg",
+//     linkElement: ".animsition-container a",
+//     inDuration: 1000,
+//     outDuration: 500
+// });
+
+$(".navbar").sticky();
+
+$(".navbar").on("sticky-start", ()=>{
+    $(".navbar").addClass("nav-red");
+});
+
+$(".navbar").on("sticky-end", ()=>{
+    $(".navbar").removeClass("nav-red");
+});
+
+$(".slides").slick({
+    fade:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true
+});
